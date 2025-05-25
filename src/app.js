031c38def98bacde26749c4bee78dd6cf23f0a2e
mrsimonnicholls <simon.nicholls@mediadesignschool.com>
2025-05-25 13:36:09 +1200

    .

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ fetch('/.netlify/functions/get-posts')
   .then(res => res.json())
   .then(posts => {
     const container = document.querySelector('#posts');
-    container.innerHTML = '';
+    
     posts.forEach(post => {
       const article = document.createElement('article');
-      article.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p>`;
+
+      const date = new Date(post.created_at).toLocaleDateString('en-NZ', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+      });
+
+      article.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.content}</p>
+        <h6>Posted on ${date}</h6>
+      `;
+
       container.appendChild(article);
     });
   });
